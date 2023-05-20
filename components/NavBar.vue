0852ef789825ef405ae33064e247eeee8b6aa4c2
Gerasjeka1985 <gekgrom@mail.ru>
2023-05-21 00:28:37 +0300

    refactor bg

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,61 +1,63 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <nav class="menu">
-    <ul class="menu__block">
-      <li class="menu__elem">
-        <NuxtLink
-            to="/"
-            class="menu__lnk"
-            exact active-class="active"
-        >
-          Главня
-        </NuxtLink>
-      </li>
-      <li class="menu__elem">
-        <NuxtLink
-            to="/about"
-            class="menu__lnk"
-            active-class="active"
-        >
-          Обо мне
-        </NuxtLink>
-      </li>
-      <li class="menu__elem">
-        <NuxtLink
-            to="/projects"
-            class="menu__lnk"
-            active-class="active"
-        >
-          Мои проекты
-        </NuxtLink>
-      </li>
-    </ul>
-  </nav>
+
+  <header class="head">
+    <burger-menu class="head__bg" />
+    <nav class="head__nv">
+      <ul class="head__block">
+        <li class="head__elem">
+          <NuxtLink
+              to="/"
+              class="head__lnk"
+              exact active-class="active"
+          >
+            Главня
+          </NuxtLink>
+        </li>
+        <li class="head__elem">
+          <NuxtLink
+              to="/about"
+              class="head__lnk"
+              active-class="active"
+          >
+            Обо мне
+          </NuxtLink>
+        </li>
+        <li class="head__elem">
+          <NuxtLink
+              to="/projects"
+              class="head__lnk"
+              active-class="active"
+          >
+            Мои проекты
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
 </template>
 
 <style scoped lang="scss">
 @import 'assets/scss/_global.scss';
 
-.active{
-  // как сделать без !important ?
-  color: orange !important;
-}
+  .active{
+    // как сделать без !important ?
+    color: orange !important;
+  }
 
-  .menu {
+  .head{
     width: 100%;
-    height: 55px;
+    height: 60px;
     background: none;
 
-    &__block{
+    &__nv{
       width: 100%;
+      height: 100%;
+      background: yellowgreen;
     }
 
-    &__elem{
-      list-style-type: none;
-    }
+    &__block{width: 100%;}
+    &__elem{list-style-type: none;}
 
     &__lnk{
       text-decoration: none;
@@ -66,16 +68,22 @@
       transition: ease-in 0.2s;
     }
   }
-@include breakpoint(large) {
-  .menu {
-    width: 100%;
-    height: 55px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: none;
 
-    &__block{
+
+@include breakpoint(large) {
+
+  .head {
+
+    &__nv {
+      width: 100%;
+      height: 55px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: none;
+    }
+
+    &__block {
       width: 50%;
       height: 100%;
       display: flex;
@@ -83,7 +91,7 @@
       justify-content: space-evenly;
     }
 
-    &__elem{
+    &__elem {
       width: 18%;
       outline: none;
       display: flex;
@@ -91,9 +99,8 @@
       align-items: center;
     }
 
-    &__lnk{
-
-      &:hover{
+    &__lnk {
+      &:hover {
         font-size: 25px;
       }
     }
